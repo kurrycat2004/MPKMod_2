20de3085043f3af58bec61d5a76c1a35081a6570
0hnezahn <benjamin.mueller2@uni-rostock.de>
2022-08-23 00:04:35 +0200

    update JSONConfig.java

--- src/main/java/io/github/kurrycat/mpkmod/util/JSONConfig.java
+++ src/main/java/io/github/kurrycat/mpkmod/util/JSONConfig.java
@@ -1,6 +1,18 @@
 package io.github.kurrycat.mpkmod.util;
 
+import java.io.File;
+
 public class JSONConfig {
-    public static final String configPath = "config/mpkmod.json";
+
+    public static final String configPath = "config/";
+    public static final String configFilePath = "config/mpk-config.json";
+
+    public static File configFile;
+
+    public static void setupFile() {
+        File dir = new File(configPath);
+        if (!dir.exists()) dir.mkdir();
+        configFile = new File(configFilePath);
+    }
 
 }
