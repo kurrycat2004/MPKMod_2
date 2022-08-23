5b3ade5dd866a31afbe05adfe8e7ae86ef30b83d
0hnezahn <benjamin.mueller2@uni-rostock.de>
2022-08-23 14:09:28 +0200

    fix paths in JSONConfig.java
    (cherry picked from commit 4cc7037e4465e2efed6b08397db5c585bde64b45)

--- src/main/java/io/github/kurrycat/mpkmod/util/JSONConfig.java
+++ src/main/java/io/github/kurrycat/mpkmod/util/JSONConfig.java
@@ -4,14 +4,15 @@ import java.io.File;
 
 public class JSONConfig {
 
-    public static final String configPath = "config/";
-    public static final String configFilePath = "config/mpk-config.json";
+    public static final String fileName = "mpk-config.json";
+    public static final String configPath = "mods/mpk/config/";
+    public static final String configFilePath = configPath + fileName;
 
     public static File configFile;
 
     public static void setupFile() {
         File dir = new File(configPath);
-        if (!dir.exists()) dir.mkdir();
+        if (!dir.exists()) dir.mkdirs();
         configFile = new File(configFilePath);
     }
 
