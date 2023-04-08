bc623a04c86f485a6b1fe8f75926ac3e38899253
JustAlittleWolf <54244277+JustAlittleWolf@users.noreply.github.com>
2023-04-08 15:51:35 +0200

    Updated OptionListItem
    Added type logging for option registry
    Extracted OptionListItem into its own abstract class
    Fixed "Reset All" button
    Added option support for String, Integer and a fallback option that just displays the value
    
    Known issue: changing the integer value to an illegal value (e.g. 2.45) crashes the game

--- src/main/java/io/github/kurrycat/mpkmod/gui/screens/options_gui/Option.java
+++ src/main/java/io/github/kurrycat/mpkmod/gui/screens/options_gui/Option.java
@@ -79,7 +79,7 @@ public class Option {
             Option option = new Option(name, value, value, type).setCategory(category).link(f);
             optionMap.put(name, option);
 
-            API.LOGGER.info("Option added to map: {} with default value: {}", name, value);
+            API.LOGGER.info("Option of type {} added to map: {} with default value: {}", type, name, value);
         }
 
         List<Tuple<ChangeListener, Method>> listeners = ClassUtil.getMethodAnnotations(classes, ChangeListener.class);
@@ -163,6 +163,10 @@ public class Option {
         switch (this.type) {
             case BOOLEAN:
                 return getBoolean();
+            case INTEGER:
+                return getInteger();
+            case DOUBLE:
+                return getDouble();
             default:
                 return getValue();
         }
@@ -178,6 +182,18 @@ public class Option {
         if (ValueType.BOOLEAN.equals(this.type))
             this.setValue(value.toString());
         return this;
+    }
+
+    public Integer getInteger() {
+        if(this.type == ValueType.INTEGER)
+            return Integer.parseInt(this.value);
+        return 0;
+    }
+
+    public Double getDouble() {
+        if(this.type == ValueType.DOUBLE)
+            return Double.parseDouble(this.value);
+        return 0.0;
     }
 
     public String getCategory() {
@@ -239,6 +255,9 @@ public class Option {
         COLOR;
     }
 
+    /**
+     * Marks a field as an MPK-Option. The field has to be public.
+     */
     @Retention(RetentionPolicy.RUNTIME)
     @Target(ElementType.FIELD)
     public @interface Field {
