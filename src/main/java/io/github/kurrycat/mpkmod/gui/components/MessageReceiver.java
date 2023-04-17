708e20b3620aff8274233e0b5198511d3cda6c77
kurrycat <kurrycat2004@gmail.com>
2023-04-17 21:21:00 +0200

    trying to fix 1.19 branch3

--- src/main/java/io/github/kurrycat/mpkmod/gui/components/MessageReceiver.java
+++ src/main/java/io/github/kurrycat/mpkmod/gui/components/MessageReceiver.java
@@ -1,5 +1,5 @@
 package io.github.kurrycat.mpkmod.gui.components;
 
 public interface MessageReceiver {
-    void postMessage(String receiverID, String content);
+    void postMessage(String receiverID, String content, boolean highlighted);
 }
