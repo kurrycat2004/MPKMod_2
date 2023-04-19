2d23bc055a4d0bf37dc588f58c3b0ea70b94265b
0hnezahn <benjamin.mueller2@uni-rostock.de>
2023-04-19 14:29:55 +0200

    fix world to file pkc support

--- src/main/java/io/github/kurrycat/mpkmod/gui/screens/main_gui/MapOverviewPane.java
+++ src/main/java/io/github/kurrycat/mpkmod/gui/screens/main_gui/MapOverviewPane.java
@@ -6,19 +6,14 @@ import io.github.kurrycat.mpkmod.compatability.MCClasses.Renderer2D;
 import io.github.kurrycat.mpkmod.compatability.MCClasses.WorldInteraction;
 import io.github.kurrycat.mpkmod.gui.components.Button;
 import io.github.kurrycat.mpkmod.gui.components.*;
-import io.github.kurrycat.mpkmod.util.MathUtil;
-import io.github.kurrycat.mpkmod.util.StringUtil;
-import io.github.kurrycat.mpkmod.util.Vector2D;
-import io.github.kurrycat.mpkmod.util.Vector3D;
+import io.github.kurrycat.mpkmod.util.*;
 
 import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class MapOverviewPane extends Pane {
     public MapOverviewPane(Vector2D pos, Vector2D size) {
@@ -36,84 +31,7 @@ public class MapOverviewPane extends Pane {
         int padding = 10;
 
         components.add(new TextLabel("Test Label", this.getDisplayedPos().add(50)));
-        components.add(new Button("TEST", getDisplayedPos().add(50, 100), new Vector2D(50, 20), mouseButton -> {
-            File dir = new File("config/mpk/test/");
-            if (!dir.exists()) dir.mkdir();
-            File csvOutputFile = new File("config/mpk/test/test.bcsv");
-
-            Player player = Player.getLatest();
-            if (player == null) return;
-
-            Vector3D playerPos = player.getPos().floor();
-
-            try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
-                pw.println("X,Y,Z,TYPE,TIER,COLOR,TOP,BOTTOM,NORTH,EAST,SOUTH,WEST");
-                for (int i = -5; i <= 5; i++) {
-                    for (int j = -5; j <= 5; j++) {
-                        for (int k = -5; k <= 5; k++) {
-                            String blockName = WorldInteraction.getBlockName(playerPos.add(i, j, k));
-                            if (blockName == null) continue;
-
-                            HashMap<String, String> properties = WorldInteraction.getBlockProperties(playerPos.add(i, j, k));
-                            if (properties == null) continue;
-
-                            if (blockName.startsWith("minecraft:")) blockName = blockName.substring(10);
-
-                            if (blockName.equals("air")) continue;
-                            else if (blockName.contains("stair")) blockName = "Stair";
-                            else if (blockName.equals("piston")) blockName = "PistonBase";
-                            else if (blockName.contains("pane")) blockName = "Pane";
-                            else if (blockName.contains("wall")) blockName = "Cobblewall";
-                            else if (blockName.equals("waterlily")) blockName = "Lilypad";
-                            else if (blockName.equals("snow_layer")) blockName = "Snow";
-                            else if (blockName.contains("chest")) blockName = "Enderchest";
-                            else if (blockName.equals("soul_sand")) blockName = "Soulsand";
-                            else if (blockName.contains("skull")) blockName = "Head";
-                            else if (blockName.equals("cocoa")) blockName = "CocoaBean";
-                            else if (blockName.equals("web")) blockName = "Cobweb";
-                            else if (blockName.equals("flower_pot")) blockName = "Flowerpot";
-
-                            else
-                                blockName = Arrays.stream(blockName.split("_")).map(StringUtil::capitalize).collect(Collectors.joining(""));
-
-                            List<String> blocks = Arrays.asList("Stair", "PistonHead", "PistonBase", "CocoaBean", "Snow", "Cake", "EndPortalFrame", "Head", "Water", "Lava", "Cobweb", "Slime", "Enderchest", "Anvil", "Bed", "BrewingStand", "Cactus", "Carpet", "Cauldron", "Cobblewall", "DragonEgg", "Fence", "Flowerpot", "Hopper", "Ice", "Lilypad", "Soulsand", "Trapdoor", "Pane", "Ladder", "Vine", "StandardBlock");
-
-                            if (!blocks.contains(blockName)) {
-                                blockName = "StandardBlock";
-                            }
-
-                            int tier = 0;
-                            if (properties.containsKey("age")) tier = MathUtil.parseInt(properties.get("age"), 0);
-                            else if(properties.containsKey("bites")) tier = MathUtil.parseInt(properties.get("bites"), 0);
-                            else if(properties.containsKey("layers")) tier = MathUtil.parseInt(properties.get("layers"), 0);
-
-                            boolean top = properties.containsKey("top") && properties.get("top").equals("true") ||
-                                    properties.containsKey("facing") && properties.get("facing").equals("top");
-                            boolean bottom = properties.containsKey("bottom") && properties.get("bottom").equals("true") ||
-                                    properties.containsKey("facing") && properties.get("facing").equals("bottom");
-                            boolean north = properties.containsKey("north") && properties.get("north").equals("true") ||
-                                    properties.containsKey("facing") && properties.get("facing").equals("north");
-                            boolean east = properties.containsKey("east") && properties.get("east").equals("true") ||
-                                    properties.containsKey("facing") && properties.get("facing").equals("east");
-                            boolean south = properties.containsKey("south") && properties.get("south").equals("true") ||
-                                    properties.containsKey("facing") && properties.get("facing").equals("south");
-                            boolean west = properties.containsKey("west") && properties.get("west").equals("true") ||
-                                    properties.containsKey("facing") && properties.get("facing").equals("west");
-
-                            pw.println(
-                                    String.format(
-                                            "%d,%d,%d,%s,%d,0xd3d3d3ff,%b,%b,%b,%b,%b,%b",
-                                            i, j, k, blockName, tier, top, bottom, north, east, south, west
-                                    )
-                            );
-                        }
-                    }
-                }
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-
-        }));
+        components.add(new Button("TEST", getDisplayedPos().add(50, 100), new Vector2D(50, 20), mouseButton -> WorldToFile.parseWorld()));
 
         components.add(new CheckButton(getDisplayedPos().add(110, 100), System.out::println));
 
---
+++ src/main/java/io/github/kurrycat/mpkmod/util/WorldToFile.java
@@ -0,0 +1,129 @@
+package io.github.kurrycat.mpkmod.util;
+
+import io.github.kurrycat.mpkmod.compatability.MCClasses.Player;
+import io.github.kurrycat.mpkmod.compatability.MCClasses.WorldInteraction;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.List;
+
+public class WorldToFile {
+
+    private static String blockName;
+    private static String blockColor;
+
+    public static void parseWorld() {
+        File dir = new File("config/mpk/test/");
+        if (!dir.exists()) dir.mkdir();
+        File csvOutputFile = new File("config/mpk/test/test.bcsv");
+
+        Player player = Player.getLatest();
+        if (player == null) return;
+
+        Vector3D playerPos = player.getPos().floor();
+
+        try (PrintWriter pw = new PrintWriter(csvOutputFile)) {
+            pw.println("X,Y,Z,TYPE,TIER,COLOR,TOP,BOTTOM,NORTH,EAST,SOUTH,WEST");
+            for (int i = -5; i <= 5; i++) {
+                for (int j = -5; j <= 5; j++) {
+                    for (int k = -5; k <= 5; k++) {
+                        blockName = WorldInteraction.getBlockName(playerPos.add(i, j, k));
+                        if (blockName == null) continue;
+
+                        HashMap<String, String> properties = WorldInteraction.getBlockProperties(playerPos.add(i, j, k));
+                        if (properties == null) continue;
+
+                        List<BoundingBox3D> bbs = WorldInteraction.getCollisionBoundingBoxes(playerPos.add(i, j, k));
+                        if (bbs.isEmpty()) continue;
+
+                        if (blockName.startsWith("minecraft:")) blockName = blockName.substring(10);
+                        String wood = "0xc8961eff";
+                        String darkIron = "0x272727ff";
+                        String stone = "0x696969ff";
+                        String plant = "0x6fd77eff";
+
+                        if (blockName.equals("air")) continue;
+                        else if (blockName.contains("anvil")) applyValues("Anvil", darkIron);
+                        else if (blockName.contains("bed")) applyValues("Bed", "0xff6a6aff");
+                        else if (blockName.contains("brewing")) applyValues("BrewingStand", "0xfff39aff");
+                        else if (blockName.contains("cactus")) applyValues("Cactus", "0x008323ff");
+                        else if (blockName.contains("cake")) applyValues("Cake", "0xffc4c4ff");
+                        else if (blockName.contains("carpet")) applyValues("Carpet", "0x9b9b9bff");
+                        else if (blockName.contains("cauldron")) applyValues("Cauldron", darkIron);
+                        else if (blockName.contains("dragon")) applyValues("DragonEgg", "0x7557ffff");
+                        else if (blockName.contains("end_portal")) applyValues("EndPortalFrame", "0xd8d92dff");
+                        else if (blockName.contains("hopper")) applyValues("Hopper", darkIron);
+                        else if (blockName.contains("ice")) applyValues("Ice", "0x97fff2ff");
+                        else if (blockName.contains("ladder")) applyValues("Ladder", wood);
+                        else if (blockName.contains("lava")) applyValues("Lava", "0xd9343455");
+                        else if (blockName.equals("piston_head")) applyValues("PistonHead", wood);
+                        else if (blockName.contains("slime")) applyValues("Slime", "0x009915ff");
+                        else if (blockName.contains("vine")) applyValues("Vine", plant);
+                        else if (blockName.contains("water")) applyValues("Water", "0x0a46a55");
+                        else if (blockName.contains("stair")) applyValues("Stair", stone);
+                        else if (blockName.contains("piston")) applyValues("PistonBase", stone);
+                        else if (blockName.contains("pane")) applyValues("Pane", "0xc3c3c388");
+                        else if (blockName.contains("wall")) applyValues("Cobblewall", stone);
+                        else if (blockName.equals("waterlily")) applyValues("Lilypad", plant);
+                        else if (blockName.equals("snow_layer")) applyValues("Snow", "0xffffffff");
+                        else if (blockName.contains("chest")) applyValues("Enderchest", "0x5239c5ff");
+                        else if (blockName.equals("soul_sand")) applyValues("Soulsand", "0x523600ff");
+                        else if (blockName.contains("skull")) applyValues("Head", "0xb9b9b9ff");
+                        else if (blockName.contains("cocoa")) applyValues("CocoaBean", "0x7b5100ff");
+                        else if (blockName.equals("web")) applyValues("Cobweb", "0xaaaaaa55");
+                        else if (blockName.equals("flower_pot")) applyValues("Flowerpot", "0xd24a00ff");
+                        else if (blockName.contains("trapdoor")) applyValues("Trapdoor", wood);
+                        else if (blockName.contains("fence")) applyValues("Fence", wood);
+                        else if (blockName.contains("slab")) applyValues("Stair", stone);
+                        else applyValues("StandardBlock", stone);
+
+                        int tier = 0;
+                        if (properties.containsKey("age")) tier = MathUtil.parseInt(properties.get("age"), 0);
+                        else if(properties.containsKey("bites")) tier = MathUtil.parseInt(properties.get("bites"), 0);
+                        else if(properties.containsKey("layers")) tier = MathUtil.parseInt(properties.get("layers"), 0);
+
+                        boolean top = (!blockName.equals("Trapdoor") && properties.containsKey("half") && properties.get("half").equals("top")) ||
+                                properties.containsKey("facing") && properties.get("facing").equals("up");
+                        boolean bottom = (!blockName.equals("Trapdoor") && properties.containsKey("half") && properties.get("half").equals("bottom")) ||
+                                properties.containsKey("facing") && properties.get("facing").equals("down");
+                        boolean north = properties.containsKey("north") && properties.get("north").equals("true") ||
+                                properties.containsKey("facing") && properties.get("facing").equals("north");
+                        boolean east = properties.containsKey("east") && properties.get("east").equals("true") ||
+                                properties.containsKey("facing") && properties.get("facing").equals("east");
+                        boolean south = properties.containsKey("south") && properties.get("south").equals("true") ||
+                                properties.containsKey("facing") && properties.get("facing").equals("south");
+                        boolean west = properties.containsKey("west") && properties.get("west").equals("true") ||
+                                properties.containsKey("facing") && properties.get("facing").equals("west");
+
+                        if (blockName.equals("PistonBase")) {
+                            if (top) {
+                                top = false;
+                                bottom = true;
+                            } else if (bottom) {
+                                top = true;
+                                bottom = false;
+                            }
+                        }
+
+                        pw.println(
+                                String.format(
+                                        "%d,%d,%d,%s,%d,%s,%b,%b,%b,%b,%b,%b",
+                                        i, j, k, blockName, tier, blockColor, top, bottom, north, east, south, west
+                                )
+                        );
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void applyValues(String name, String color) {
+        blockName = name;
+        blockColor = color;
+    }
+
+}
